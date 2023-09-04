276ca7a35e05a90e5d91d2ee5eeb0c8f5170ddf0
Valyria <76444716+itsValyria@users.noreply.github.com>
2023-09-04 22:36:13 +0200

    Initial draft

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+// const expandButton = document.querySelector('header button')
+// expandButton.addEventListener('click', expand)
 
-function expand () {
-  document.body.classList.toggle('expand')
+// function expand () {
+//   document.body.classList.toggle('expand')
+// }
+
+const expandButton = document.querySelectorAll('.tile__button');
+expandButton.forEach((button) => {
+  button.addEventListener('click', expand.bind(this, button));
+})
+
+function expand(button) {
+  button.parentElement.classList.toggle('tile--fullscreen');
 }
+
+// Reef & Co.
+// Voor al uw webdesign. En meer... ;)
