ae1026bc461a46a3b17fcfa933453c62d85e71bc
Valyria <76444716+itsValyria@users.noreply.github.com>
2023-09-06 12:39:33 +0200

    initial commit 2
    changed layouts, need to fix position and emojis

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,13 @@
 //   document.body.classList.toggle('expand')
 // }
 
-const expandButton = document.querySelectorAll('.tile__button');
-expandButton.forEach((button) => {
-  button.addEventListener('click', expand.bind(this, button));
-})
+// const expandButton = document.querySelectorAll('.tile__button');
+// expandButton.forEach((button) => {
+//   button.addEventListener('click', expand.bind(this, button));
+// })
 
-function expand(button) {
-  button.parentElement.classList.toggle('tile--fullscreen');
+// function expand(button) {
+//   button.parentElement.classList.toggle('tile--fullscreen');
 }
 
 // Reef & Co.
